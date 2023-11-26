a6b0130f7d7a5606f4f666a22365ca586298cec5
Leefranklyn <124352547+Leefranklyn@users.noreply.github.com>
2023-11-26 08:28:25 -0700

    Update admin.routes.js

--- src/routes/admin/admin.routes.js
+++ src/routes/admin/admin.routes.js
@@ -2,13 +2,14 @@ import { Router } from "express";
 import { verifyToken } from "../../middlewares/jwt.js";
 import upload from "../../config/multer.js";
 import { adminLogin, adminRegistration } from "../../controllers/auth/admin.auth.controller.js";
-import { addPyhsicalUserAndBorrowBook, disableUser, getAdmin, getAllUsers, getUsersWithBorrowedBooks, getUsersWithBorrowedBooksDue, updateAdmin, uploadAdminProfilePhoto, verifyPayment } from "../../controllers/admin/admin.controller.js";
+import { addPyhsicalUserAndBorrowBook, disableUser, getAdmin, getAllUsers, searchForUser, getUsersWithBorrowedBooks, getUsersWithBorrowedBooksDue, updateAdmin, uploadAdminProfilePhoto, verifyPayment } from "../../controllers/admin/admin.controller.js";
 const router = Router();
 
 router.post("/signup", adminRegistration);
 router.post("/login", adminLogin);
 router.get("/", verifyToken("admin"), getAdmin);
 router.get("/users", verifyToken("admin"), getAllUsers);
+router.get("/users/:regno", verifyToken("admin"), searchForUser);
 router.get("/users/borrowed", verifyToken("admin"), getUsersWithBorrowedBooks);
 router.get("/users/borrowed/due", verifyToken("admin"), getUsersWithBorrowedBooksDue);
 router.post("/uploadprofile",  verifyToken("admin"), upload("admin").single("profilePhoto"), uploadAdminProfilePhoto);
